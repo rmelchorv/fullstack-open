ccf645cc616ac7716a2175c75979aec0f460464b
Ricardo E. Melchor <rmelchorv@gmail.com>
2025-10-17 17:26:44 -0600

    Firt component: props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,22 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
+
   return (
     <div>
-      <p>Hello world!</p>
+      <p>Hello {props.name}, you're {props.age} years old!</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = "Melchor"
+  const age = 46
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Ricardo" age={28+18} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
