dcd0b0438b519c336f3b2c59df367e75d5032ec7
Ricardo E. Melchor <rmelchorv@gmail.com>
2025-10-17 17:06:03 -0600

    First component: dynamic content

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,16 @@
 const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log("Hello from component!")
+  console.log(now, a+b)
+
   return (
     <div>
-      <p>Hello world!!</p>
+      <p>Hello world! It's {now.toString()}</p>
+      <p>
+        {a} plus {b} equals {a+b}
+      </p>
     </div>
   )
 }
