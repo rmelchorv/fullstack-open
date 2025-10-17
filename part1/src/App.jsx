14110af9e453c726b123c8984f5a57ed4dabceb0
Ricardo E. Melchor <rmelchorv@gmail.com>
2025-10-17 17:21:29 -0600

    First component: multiple components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,18 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log("Hello from component!")
-  console.log(now, a+b)
-
+const Hello = () => {
   return (
     <div>
-      <p>Hello world! It's {now.toString()}</p>
-      <p>
-        {a} plus {b} equals {a+b}
-      </p>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
